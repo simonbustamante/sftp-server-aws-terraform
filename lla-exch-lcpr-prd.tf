e8d42e4d10246562b111dca39e403f6a12673bf9
Simon Bustamante <simon.bustamante@gmail.com>
2024-09-20 09:25:36 -0500

    last review role and policy destroy protection

--- lla-exch-lcpr-prd.tf
+++ lla-exch-lcpr-prd.tf
@@ -2,7 +2,7 @@ locals {
   profile_prd = "876881173184_LLA-DL_DevOps"
   region_prd = "us-east-1"
   server_name_tag_prd = "sftp_lcpr_plume_prd"
-  role_prd = "arn:aws:iam::876881173184:role/lla-exch-lcpr-prod-transfer-family-role"
+  # role_prd = "arn:aws:iam::876881173184:role/lla-exch-lcpr-prod-transfer-family-role"
   user_name_prd = "sftp_lcpr_plume_prd_user"
   entry_prd = "/lcpr/plume"
   target_prd = "/source.lcpr.prod/lcpr/plume"
@@ -15,6 +15,35 @@ provider "aws" {
   alias   = "prd"
   profile = local.profile_prd
   region  = local.region_prd
+}
+
+# create role
+resource "aws_iam_role" "transfer_family_role_prd" {
+  provider = aws.prd
+  name               = "lla-exch-lcpr-prod-transfer-family-role"
+  assume_role_policy = file("lla-exch-lcpr-prod-transfer-family-role.json")
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# create policy
+resource "aws_iam_policy" "transfer_family_policy_prd" {
+  provider = aws.prd
+  name        = "lla-exch-lcpr-prod-transfer-family-policy"
+  description = "IAM policy for Transfer Family role to access S3 bucket and manage Transfer resources"
+
+  policy = file("lla-exch-lcpr-prod-transfer-family-policy.json")
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Attach policy to role
+resource "aws_iam_role_policy_attachment" "transfer_family_policy_attachment_prd" {
+  provider  = aws.prd
+  role      = aws_iam_role.transfer_family_role_prd.name
+  policy_arn = aws_iam_policy.transfer_family_policy_prd.arn
 }
 
 resource "aws_transfer_server" "sftp_server_prd" {
@@ -32,7 +61,7 @@ resource "aws_transfer_server" "sftp_server_prd" {
 resource "aws_transfer_user" "sftp_user_prd" {
     provider          = aws.prd
     server_id         = aws_transfer_server.sftp_server_prd.id
-    role              = local.role_prd
+    role              = aws_iam_role.transfer_family_role_prd.arn
     user_name         = local.user_name_prd 
     
 
